049949eac59546320a8c03b9ea51f7ea9e370cfe
bgunness <brandon.gunness@gmail.com>
2020-09-17 23:22:49 -0400

    Added fontawesome module, included burger icon in header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 function Header() {
+
+const barIcon = <FontAwesomeIcon icon = {faBars} />
+
     return(
         <div className='header'>
             <div className='brand'>
                 <h2 className='main'>ZED</h2>
                 <h3 className='sub'>Art</h3>
             </div>
-            <div className='dropdown test'>
-                <span>Burger</span>
+            <div className='dropdown'>
+                <span>{barIcon}</span>
                     <div>
                         <ul className='dropdown-content'>
                             <li>Gallery</li>
