e1ca5eb4fb42d2891df1f83cd98fc733608091f4
bgunness <brandon.gunness@gmail.com>
2020-09-19 14:36:24 -0400

    Added key prop to image, added map function to gallery-body render

--- components/Gallery-Body.js
+++ components/Gallery-Body.js
@@ -21,8 +21,18 @@ class GalleryBody extends React.Component {
     render() {
         return(
             <div className='gallery-image'>
-                {this.state.images.length > 0 ? <Image images={this.state.images}/> : <img className='image' src={this.state.defaultSrc} />}
+                {this.state.images.length > 0 ? 
+                    this.state.images.map(image => <Image image={image} key={image.id} />) : 
+                    <img className='image' src={this.state.defaultSrc} />
+                }
             </div>
+
+            // <div className='gallery-image'>
+            //     {this.state.images.length > 0 ? 
+            //         <Image images={this.state.images}/> : 
+            //         <img className='image' src={this.state.defaultSrc} />
+            //     }
+            // </div>
         )
     }
 }
--- components/Image.js
+++ components/Image.js
@@ -1,15 +1,16 @@
 function Image(props) {
-    console.log(props.images)
-    
-// Define function here that iterates props.images, and creates a div like below to print each image on each iteration
     return (
-        props.images.map(image => {
-            return(
-                <div>
-                    <img className='image' src={image.src} alt={image.description} key={image.id}/>
-                </div>
-            )
-        })
+            <div>
+                <img className='image' src={props.image.src} alt={props.image.description} />
+            </div>
+
+        // props.images.map(image => {
+        //     return(
+        //         <div>
+        //             <img className='image' src={image.src} alt={image.description} />
+        //         </div>
+        //     )
+        // })
     )
 }
 
